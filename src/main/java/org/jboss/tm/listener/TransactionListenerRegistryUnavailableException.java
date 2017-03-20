24001ac7779f19ce68fb4b54e10d121659f573c0
mmusgrov <mmusgrov@redhat.com>
2017-03-20 13:01:02 +0000

    JBTM-2869 Deprecate transaction association events

--- src/main/java/org/jboss/tm/listener/TransactionListenerRegistryUnavailableException.java
+++ src/main/java/org/jboss/tm/listener/TransactionListenerRegistryUnavailableException.java
@@ -22,9 +22,12 @@
 package org.jboss.tm.listener;
 
 /**
+ * @deprecated WFTC (https://issues.jboss.org/projects/WFTC) replaces this functionality
+ *
  * An exception type thrown by {@link org.jboss.tm.listener.TransactionListenerRegistryLocator} if the registry
  * is unavailable
  */
+@Deprecated
 public class TransactionListenerRegistryUnavailableException extends Exception {
      public TransactionListenerRegistryUnavailableException(String message) {
         super(message);
