f8736162d998f9c8f1514393df5fb2d4ebfa441d
Jonathan Halliday <jonathan.halliday@redhat.com>
2020-10-12 10:05:32 +0100

    JBTM-3325 Create a way to close XAResource at the end of the recovery cycle processing
    encourage use of wrapper metadata

--- src/main/java/org/jboss/tm/XAResourceRecovery.java
+++ src/main/java/org/jboss/tm/XAResourceRecovery.java
@@ -46,7 +46,55 @@ public interface XAResourceRecovery
      * The Resource should be instantiated in such a way as to carry the necessary permissions to
      * allow transaction recovery. For some deployments it may therefore be necessary or desirable to
      * provide resource(s) based on e.g. database connection parameters such as username other than those
-     * used for the regular application connections to the same resource manager. 
+     * used for the regular application connections to the same resource manager.
+     *
+     * Note that, unlike a normal connection pool, a resource manager MAY return resource instance(s)
+     * already returned, even if not yet released.
+     * i.e. it's ok for repeated calls to this method to return the same result.
+     *
+     * @deprecated Prefer {@link #getWrappedXAResources} where possible.
      */
-    public XAResource[] getXAResources();
+    @Deprecated
+    default XAResource[] getXAResources() {
+        return getWrappedXAResources();
+    }
+
+    /**
+     * Provides wrapped XAResource(s) to the transaction system for recovery purposes.
+     *
+     * @return An array of XAResourceWrapper objects for use in transaction recovery
+     * In most cases the implementation will need to return only a single XAResource in the array.
+     * For more sophisticated cases, such as where multiple different connection types are supported,
+     * it may be necessary to return more than one.
+     *
+     * The Resource should be instantiated in such a way as to carry the necessary permissions to
+     * allow transaction recovery. For some deployments it may therefore be necessary or desirable to
+     * provide resource(s) based on e.g. database connection parameters such as username other than those
+     * used for the regular application connections to the same resource manager.
+     *
+     * A transaction manager SHOULD call this method in preference to getXAResources, subsequently
+     * calling getXAResources only if this method provides no resources.
+     *
+     * Note that, unlike a normal connection pool, a resource manager MAY return resource instance(s)
+     * already returned, even if not yet released.
+     * i.e. it's ok for repeated calls to this method to return the same result.
+     *
+     * @see XAResourceRecovery#getXAResources
+     */
+    default XAResourceWrapper[] getWrappedXAResources() {
+        return null;
+    }
+
+    /**
+     * Called by the transaction system to return a XAResource, previously provided by a getter, to the provider.
+     * A released resource will no longer be used by the transaction system and the resource manager may
+     * e.g. return the underlying connection to a pool or close it.
+     *
+     * Note that transaction system implementations MAY hold provided resources indefinitely,
+     * but SHOULD release them before calling a getter again to obtain more.
+     *
+     * @param xaResource An XAResource instance previously included in the values returned by getXAResources or getWrappedXAResources
+     */
+    default void release(XAResource xaResource) {
+    }
 }
